c15b600885594fdae18bc0b858c1149b4d4fda10
Peter Ayeni <peter.ayeni@previsico.com>
2020-11-23 10:11:33 +0000

    chore: clean up code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-
 import './App.css';
 
 function App() {
-  const position = [51.505, -0.09];
+  const position = [51.474, -360.08];
+
   return (
     <MapContainer
       className="main"
       center={position}
-      zoom={13}
+      zoom={6}
       scrollWheelZoom={true}
     >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
       />
+
       <Marker position={position}>
         <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
+          Welcome to. <br /> London.
         </Popup>
       </Marker>
     </MapContainer>
